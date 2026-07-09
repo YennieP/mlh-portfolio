3522c5fd6c05825eb7fb1237b851d23fa9466c2a
Yanxi Pan <pyx2816459347@gmail.com>
2026-07-09 15:56:19 -0700

    Connect to MySQL database via peewee (course task)

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,18 @@
 import os
+from peewee import *
 from flask import Flask, render_template, request, url_for
 from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
+
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host=os.getenv("MYSQL_HOST"),
+    port=3306
+)
+print(mydb)
 
 
 @app.route('/')
